0c4c48abb43a9d2a73f8674d8a46ef6f6e3ce476
Jan Krebs <krebs-jan@gmx.de>
2017-06-28 14:31:18 +0200

    Update with task.clj.

---
+++ src/clojure_austausch/task.clj
@@ -0,0 +1,21 @@
+(ns clojure-austausch.task
+  (:require
+   [schema.core :as s]))
+
+(def convention-config
+  {:required-key s/Str
+   (s/optional-key :key-with-default) s/Str
+   (s/optional-key :key-with-default-2) s/Str
+   (s/optional-key :optional-key) s/Str})
+
+(def example-convention-config
+  {:required-key "my-required-key"})
+
+(def stack-config
+  {:required-key s/Str
+   :key-with-default s/Str
+   (s/optional-key :optional-key) s/Str
+   })
+
+;; convention-config -> stack-config
+;; Hint: (assoc map key val) updates a key-value pair in the map.
